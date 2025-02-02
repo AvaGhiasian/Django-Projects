14b70dcebdb00ec793c18956915d1c08ed5779d0
Ava Ghiasian <avaghiasian82@gmail.com>
2025-02-02 11:31:50 +0330

    404 template

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.http import Http404, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
 from django.template.loader import render_to_string
 
@@ -37,8 +37,8 @@ def monthly_challenges(request, month: str):
             "month_name": month.capitalize()
             })
     except:
-        return HttpResponseNotFound("<h1>This month is not supported!</h1>")
-
+        response_data = render_to_string("404.html")
+        return HttpResponseNotFound(response_data)
 
 def monthly_challenges_by_num(request, month: int):
     try:
