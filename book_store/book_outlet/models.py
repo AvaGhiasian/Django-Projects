00e1a12366049fc910613394f5f9a2770a14e46f
Ava Ghiasian <avaghiasian@yahoo.com>
2025-02-08 18:43:38 +0330

    added models and made migrations to the DB

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=50)
+    # no rating should be higher than 5 and less than 1, so validators are needed
+    rating = models.IntegerField(
+        validators=[MinValueValidator(1), MinValueValidator(5)])
+    author = models.CharField(null=True, max_length=100)
+    is_best_selling = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"title: {self.title}, rate: {self.rating}"
