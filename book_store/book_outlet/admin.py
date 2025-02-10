60b6acf8ed21f2fb8674671f5cd75833e147aa08
Ava Ghiasian <avaghiasian@yahoo.com>
2025-02-10 23:19:59 +0330

    added admin and configurations

--- book_store/book_outlet/admin.py
+++ book_store/book_outlet/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from . models import Book
 
 # Register your models here.
+# to do more configuration in the DB, we'll add class BookAdmin.
+
+
+class BookAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title", )}
+    list_filter = ("author", "rating", )
+    list_display = ("title", "author", )
+
+
+admin.site.register(Book, BookAdmin)
--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.urls import reverse
-from django.utils.text import slugify # this imports the built in slugify helper
+# this imports the built in slugify helper
+from django.utils.text import slugify
 
 # Create your models here.
 
@@ -10,24 +11,16 @@ class Book(models.Model):
     title = models.CharField(max_length=50)
     # no rating should be higher than 5 and less than 1, so validators are needed
     rating = models.IntegerField(
-        validators=[MinValueValidator(1), MinValueValidator(5)])
+        validators=[MinValueValidator(1), MaxValueValidator(5)])
     author = models.CharField(null=True, max_length=100)
     is_best_selling = models.BooleanField(default=False)
-    slug = models.SlugField(default="", null=False, db_index=True, primary_key=True) 
+    slug = models.SlugField(default="", blank=True, null=False, db_index=True)
     # if book name is Harry Potter auto populate to harry-potter
     # db_index are a technical detail, means that DB will save the value in a way that makes searching them a bit more efficient.
     # so creating a field like this db_index=True makes searching the field quicker.
 
     def get_absolute_url(self):
         return reverse("book-detail", args=[self.slug])
-    
-    def save(self, *args, **kwargs):
-        """
-        title is taken and sat to slug filed, so this transforms the title into a slug,
-        then super().save() is called to save the data to the DB.
-        """
-        self.slug = slugify(self.title)
-        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"title: {self.title}, rate: {self.rating}"
