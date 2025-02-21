c7462578e734bd688fc885a2a44da4c43ea6983f
Ava Ghiasian <avaghiasian@yahoo.com>
2025-02-21 16:42:21 +0330

    added forms

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+from .forms import ReviewForm
 
 # Create your views here.
 
@@ -14,19 +15,38 @@ def review(request):
     instead of returning a rendered template we redirect to a different url with get req
     and that url will render a template
 
-    """
     if request.method == "POST":
-        entered_username = request.POST["username"]
+        entered_username = request.POST["username"] .POST gives us access to the key
 
         if entered_username == "" and len(entered_username) >= 100:
             return render(request, "reviews/review.html", {
                 "has_error": True
             })
-        
+
         return HttpResponseRedirect("/thank-you")
 
     return render(request, "reviews/review.html", {
         "has_error": False
+    })
+
+    """
+    if request.method == "POST":
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            """
+        form.is_valid() is defines in Form class and does 3 things:
+        1- validating the input(checking if it is not empty)
+        2- returns true if form is valid
+        3- if data is valid, it'll populate another field with that valid data
+        """
+            print(form.cleaned_data) # field that contains cleaned data 
+            return HttpResponseRedirect("/thank-you")
+    else:
+        """else it is a GET request: """
+        form = ReviewForm()
+
+    return render(request, "reviews/review.html", {
+        "form": form
     })
 
 
