97565629b7aed9ce91b42632a38ae21d6d42be3f
Ava Ghiasian <avaghiasian@yahoo.com>
2025-03-04 21:22:03 +0330

    added models, created forms

--- feedback/profiles/models.py
+++ feedback/profiles/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class UserProfile(models.Model):
+    # files should be stored in the hard instead of DB bcs they make DB slow
+    # .FileField() stores files in the hard and only stores the path in the DB
+    image = models.ImageField()
