7d1a0bfcf9d7d8530d1444ca02016e3f0ecd39a3
Ava Ghiasian <avaghiasian@yahoo.com>
2025-03-05 21:15:58 +0330

    sessions were added

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -98,3 +98,19 @@ class ReviewsListView(ListView):
 class SingleReviewView(DetailView):
     template_name = "reviews/single_review.html"
     model = Review
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        loaded_review = self.object
+        request = self.request
+        favorite_id = request.session.get("favorite_review")
+        context["is_favorite"] = favorite_id == str(loaded_review.id)
+        return context
+
+
+class AddFavoriteView(View):
+    def post(self, request):
+        review_id = request.POST["review_id"]
+        # review_id gets stored in the session with the key: favorite_review
+        request.session["favorite_review"] = review_id
+        return HttpResponseRedirect("/reviews/" + review_id)
