8847dc0ee7af22872b49195d0d314c43a013fba2
Ava Ghiasian <avaghiasian@yahoo.com>
2025-03-06 11:59:49 +0330

    added image upload field

--- weblog/blog/models.py
+++ weblog/blog/models.py
@@ -26,7 +26,7 @@ class Author(models.Model):
 class Post(models.Model):
     title = models.CharField(max_length=100)
     excerpt = models.CharField(max_length=250)
-    image_name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="posts", null=True) # upload_to="posts": uploads to posts folder in uploads folder
     date = models.DateField(auto_now=True)
     slug = models.SlugField(unique=True, db_index=True)
     content = models.TextField(validators=[MinLengthValidator(10)])
