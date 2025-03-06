166109e06d10de8a9322026363b97f6cc4964d88
Ava Ghiasian <avaghiasian@yahoo.com>
2025-03-06 22:54:28 +0330

    finally fiiiiiiiiiinished

--- weblog/blog/urls.py
+++ weblog/blog/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.start_page, name="start-page"),
-    path("posts", views.posts, name="posts-page"),
-    path("posts/<slug:slug>", views.post_details, name="post-detail-page")
+    path("", views.StartPageView.as_view(), name="start-page"),
+    path("posts", views.PostsView.as_view(), name="posts-page"),
+    path("posts/<slug:slug>", views.SinglePostView.as_view(),
+         name="post-detail-page"),
+    path("read-later", views.ReadLaterView.as_view(), name="read-later")
 ]
