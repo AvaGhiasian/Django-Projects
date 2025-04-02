6eb292496af6a10c2d530e8497d6663bb1ff5f78
Ava Ghiasian <avaghiasian@yahoo.com>
2025-04-02 19:18:51 +0330

    added serializer get request

--- movie/watchmate/watchmate/urls.py
+++ movie/watchmate/watchmate/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('movies/', include('watch_list.urls')),
+    path('movies/', include('watch_list.api.urls')),
 ]
