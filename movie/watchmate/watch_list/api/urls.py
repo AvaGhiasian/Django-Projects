44bdbb1b72a7bf6e16d457d481dd074838f6fbd8
Ava Ghiasian <avaghiasian@yahoo.com>
2025-04-03 15:31:36 +0330

    added class based views

--- movie/watchmate/watch_list/api/urls.py
+++ movie/watchmate/watch_list/api/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 from ..api import views
 
 urlpatterns = [
-    path('list/', views.movie_list, name='movie_list'),
-    path('list/<int:pk>', views.movie_detail, name='movie_detail'),
+    path('list/', views.MovieListApiView.as_view(), name='movie_list'),
+    path('list/<int:pk>', views.MovieDetailApiView.as_view(), name='movie_detail'),
 ]
