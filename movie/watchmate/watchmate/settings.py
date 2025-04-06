6587b508099fee671fdbcb20596f604bd8bd37e8
Ava Ghiasian <avaghiasian@yahoo.com>
2025-04-06 13:36:16 +0330

    added settings to generat auth token and generated token manually for now

--- movie/watchmate/watchmate/settings.py
+++ movie/watchmate/watchmate/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'watch_list',
     'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -120,6 +121,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
     ]
 }
