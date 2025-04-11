b4231a495ddffabf2e8a66dca5e15fa4b45e4378
Ava Ghiasian <avaghiasian@yahoo.com>
2025-04-11 18:51:37 +0330

    added test cases for all watchlist views

--- movie/watchmate/watch_list/tests.py
+++ movie/watchmate/watch_list/tests.py
@@ -38,3 +38,36 @@ class StreamPlatformTestCase(APITestCase):
     def test_streamplatform_individual(self):
         response = self.client.get(reverse('platform-detail', args=(self.stream_platform.pk,)))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class WatchListTestCase(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='example', password='password123')
+        self.token = Token.objects.get(user__username=self.user.username)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+
+        self.stream_platform = models.StreamPlatform.objects.create(name="Netflix", about="#1 streaming platform",
+                                                                    website="https://netflix.com")
+
+        self.watch_list = models.WatchList.objects.create(title="Example Title", description="Example Description",
+                                                          active=True, platform=self.stream_platform)
+
+    def test_watchlist_create(self):
+        data = {
+            "title": "Example Title",
+            "description": "Example Description",
+            "active": True,
+            "platform": self.stream_platform,
+        }
+        response = self.client.post(reverse('movie_list'), data)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_watchlist_list(self):
+        response = self.client.get(reverse('movie_list'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_watchlist_individual(self):
+        response = self.client.get(reverse('movie_detail', args=(self.watch_list.pk,)))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(models.WatchList.objects.count(), 1)
+        self.assertEqual(models.WatchList.objects.get().title, 'Example Title')
