2381d44b14d3db15ba07609c870fb64faead4afa
Ava Ghiasian <avaghiasian@yahoo.com>
2025-04-11 17:16:40 +0330

    added testcase for login and logout

--- movie/watchmate/user_app/tests.py
+++ movie/watchmate/user_app/tests.py
@@ -24,5 +24,25 @@ class RegisterTestCase(APITestCase):
             'password': 'password123',
             'password2': 'password123',
         }
-        response = self.client.post(reverse('register'), data) # sends a post request with data to that url
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED) # if we 201 means that we have created the user
+        response = self.client.post(reverse('register'), data)  # sends a post request with data to that url
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)  # if we 201 means that we have created the user
+
+
+class LoginLogoutTestCase(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='example', password='password123')
+
+    def test_login(self):
+        data = {
+            "username": "example",
+            "password": "password123",
+        }
+        response = self.client.post(reverse('login'), data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_logout(self):
+        """ we need to access token and pass it for logging out """
+        self.token = Token.objects.get(user__username="example")
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+        response = self.client.post(reverse('logout'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
