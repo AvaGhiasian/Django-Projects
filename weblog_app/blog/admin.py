289a2d028c9a27ee056f6437d964ed31694e675a
Ava Ghiasian <avaghiasian@yahoo.com>
2025-05-07 20:40:47 +0330

    added new app and model

--- weblog_app/blog/admin.py
+++ weblog_app/blog/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django_jalali.admin.filters import JDateFieldListFilter
+import django_jalali.admin as jadmin
 
+from .models import *
 # Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author','category', 'publish', 'status']
+    ordering = ['title', 'publish']
+    list_filter = ['status', ('publish', JDateFieldListFilter), 'author']
+    search_fields = ['title', 'description']
+    raw_id_fields = ['author']
+    date_hierarchy = 'publish'
+    prepopulated_fields = {"slug": ['title']}  # auto generating slug
+    list_editable = ['status']
+    # list_display_links = ['author']
+    # autocomplete_fields = ['author']
+    # inlines = [ImageInline, CommentInline]
