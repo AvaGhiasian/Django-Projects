db8c21fed82921b6ad0febd9915ad66dc5dfabd9
Ava Ghiasian <avaghiasian@yahoo.com>
2025-05-08 10:59:17 +0330

    changed to class based views

--- weblog_app/blog/views.py
+++ weblog_app/blog/views.py
@@ -1,6 +1,7 @@
 from django.http import Http404
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import PageNotAnInteger, Paginator, EmptyPage
+from django.views.generic import ListView, DetailView
 
 from .models import Post
 
@@ -16,25 +17,36 @@ def index(request):
     return render(request, 'blog/index.html')
 
 
-def post_list(request, category=None):
-    posts = Post.published.all()
-    paginator = Paginator(posts, 5)
-    page_number = request.GET.get('page', 1)
-    try:
-        posts = paginator.page(page_number)
-    except EmptyPage:
-        posts = paginator.page(paginator.num_pages)  # shows the last page
-    except PageNotAnInteger:
-        posts = paginator.page(1)
+# def post_list(request, category=None):
+#     posts = Post.published.all()
+#     paginator = Paginator(posts, 5)
+#     page_number = request.GET.get('page', 1)
+#     try:
+#         posts = paginator.page(page_number)
+#     except EmptyPage:
+#         posts = paginator.page(paginator.num_pages)  # shows the last page
+#     except PageNotAnInteger:
+#         posts = paginator.page(1)
+#
+#     context = {
+#         'posts': posts,
+#     }
+#     return render(request, "blog/list.html", context)
 
-    context = {
-        'posts': posts,
-    }
-    return render(request, "blog/list.html", context)
+class PostListView(ListView):
+    # model = Post  by default takes Post.objects.all()
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 5
+    template_name = "blog/list.html"
 
-def post_detail(request, id):
-    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
-    context = {
-        'post': post,
-    }
-    return render(request, "blog/detail.html", context)
+# def post_detail(request, id):
+#     post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+#     context = {
+#         'post': post,
+#     }
+#     return render(request, "blog/detail.html", context)
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "blog/detail.html"
